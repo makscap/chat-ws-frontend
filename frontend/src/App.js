dcfe8c525286d5f2a6bab3ed206aa9f5ee6e7b24
makscap <63607294+makscap@users.noreply.github.com>
2023-01-21 16:20:08 +0100

    finished rooms of Chat

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,26 +1,10 @@
-import { useState } from "react";
 import "./App.css";
 import Chat from "./Chat/Chat";
 
 function App() {
-  const [isAuth, setIsAuth] = useState(false);
-
   return (
     <div className=" bg-[#000000] p-[20px]">
-      {isAuth ? (
-        <Chat />
-      ) : (
-        <div>
-          <form>
-            <label for="name">Name:</label>
-            <input type="text" name="name" />
-            <label for="companion">Companion:</label>
-            <input type="text" name="companion" />
-
-            <button onClick={setIsAuth(!isAuth)}>JOIN</button>
-          </form>
-        </div>
-      )}
+      <Chat />
     </div>
   );
 }
--- frontend/src/Chat/Chat.js
+++ frontend/src/Chat/Chat.js
@@ -1,45 +1,75 @@
 import Rooms from "../Rooms/Rooms";
 import Messages from "../Messages/Messages";
 import s from "./Chat.module.css";
-import { useEffect, useState } from "react";
+import { useEffect, useReducer, useState } from "react";
+
+function reducer(state, action) {
+  const rooms = new Set(state)
+  switch (action.type) {
+    case 'rooms': {
+      action.rooms.forEach(room => rooms.add(room));
+      return Array.from(rooms);
+    }
+    case 'new-room': {
+      rooms.add(action.room);
+      return Array.from(rooms);
+    }
+    case 'delete-room': {
+      rooms.delete(action.room);
+      return Array.from(rooms);
+    }
+    default: {
+      console.log('Unknown action: ' + action.type);
+    }
+  }
+}
 
 function Chat() {
   const [connected, setConnected] = useState(false);
-  const [rooms, setRooms] = useState([]);
+  const [state, dispatch] = useReducer(reducer, [])
+
 
   useEffect(() => {
-    const userEmail = localStorage.getItem("email");
-    const webSocket = new WebSocket(`ws://127.0.0.1:8080/?email=${userEmail}`);
+    function connect () {
+      const userEmail = localStorage.getItem("email");
+      const webSocket = new WebSocket(`ws://127.0.0.1:8080/?email=${userEmail}`);
 
-    webSocket.onopen = function (data) {
-      console.log(data);
-      setConnected(true);
-      webSocket.send(JSON.stringify({ event: "rooms" }));
-      webSocket.onmessage = (event) => {
-        const eventData = JSON.parse(event.data);
+      webSocket.onopen = function () {
+        setConnected(true);
+        console.log('Connected!')
 
-        console.log(eventData);
-
-        if (eventData.event === "rooms") {
-          setRooms((prev) => {
-            console.log("oldValue", prev);
-            return eventData.rooms;
-          });
-        }
-        if (eventData.event === "new-room") {
-          setRooms((prev) => {
-            console.log("oldValue-new", prev);
-            return [...rooms, eventData.room];
-          });
-        }
+        webSocket.onmessage = (event) => {
+          const eventData = JSON.parse(event.data);
+          if (eventData.type === 'room') {
+            dispatch({
+              type: eventData.event,
+              rooms: eventData.rooms || [],
+              room: eventData.room || [],
+            })
+          }
+        };
       };
-    };
+
+      webSocket.onclose = function(e) {
+        console.log('Socket is closed. Reconnect will be attempted in 1 second.', e.reason);
+        setTimeout(function() {
+          connect();
+        }, 1000);
+      };
+    
+      webSocket.onerror = function(err) {
+        console.error('Socket encountered error: ', err.message, 'Closing socket');
+        webSocket.close();
+      };
+    }
+
+    connect();
   }, []);
 
   return (
     <div className={s.chat}>
       <div className="w-[300px] border-1 border-black border-r ">
-        {connected && rooms && <Rooms rooms={rooms} />}
+        {connected && <Rooms rooms={state} />}
       </div>
       <div className="flex-1 text-left ">{connected && <Messages />}</div>
     </div>
